57737a224ede68eec65e2a792a595dceab163f0c
AR102 <weltraumkugel@gmail.com>
2023-07-06 13:38:00 +0200

    added german language files

---
+++ lang/de/lang.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * German language file for latexcaption plugin
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Ben van Magill <ben.vanmagill16@gmail.com>
+ * @author  Till Biskup <till@till-biskup>
+ * @author  Alexander Reimer <alexander.reimer2357@gmail.com>
+ */
+
+$lang['figurelong'] = 'Abbildung';
+$lang['figureabbrev'] = 'Abb.';
+$lang['tablelong'] = 'Tabelle';
+$lang['tableabbrev'] = 'Tab.';
+$lang['codeblocklong'] = 'Quellcode';
+$lang['codeblockabbrev'] = 'Quellcode.';
+$lang['fileblocklong'] = 'Quellcode';
+$lang['fileblockabbrev'] = 'Quellcode.';
+$lang['figure'] = 'Füge Abbildung mit Unterschrift hinzu';
+$lang['table'] = 'Füge Tabelle mit Unterschrift hinzu';
+$lang['code'] = 'Füge Quellcode mit Unterschrift hinzu';
+$lang['file'] = 'Füge Quelldatei mit Unterschrift hinzu';
+$lang['reference'] = 'Referenz zu Abbildung/Tabelle';
+$lang['latexcaption'] = 'Latex Unterschriften';
+
+//Setup VIM: ex: et ts=4 :
---
+++ lang/de/settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * DokuWiki Plugin latexcaption Settings
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Ben van Magill <ben.vanmagill16@gmail.com>
+ * @author  Alexander Reimer <alexander.reimer2357@gmail.com>
+ */
+
+// keys need to match the config setting name
+$lang['abbrev'] = 'Sollen Abkürzungen (Abb., Tab.) anstelle von ausgeschriebenen Namen (Abbildung, Tabelle) in Unterschriften verwendet werden?';
+$lang['captiontag'] = 'Tag für Unterschriften. Ändern Sie diesen, wenn der caption Tag in Konflikt mit Ihrem Template steht.';
+
+//Setup VIM: ex: et ts=4 :
